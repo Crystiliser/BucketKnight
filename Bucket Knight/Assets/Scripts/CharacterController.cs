b8973edc9ef5425a9b7ecea219c768211ea61f39
Marc Taurasi <marcataurasi@gmail.com>
2019-02-03 22:44:44 -0700

    Update Unity Version 2018.3.0f2 Import Standard Assets Create Asset Showcase Scene Create Enviorment Models

---
+++ Bucket Knight/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+
+	[SerializeField]
+	private float speed = 10f;
+
+	void Start ()
+	{
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+
+	void Update ()
+	{
+		float translation = Input.GetAxis("Vertical") * speed;
+		float straffe = Input.GetAxis("Horizontal") * speed;
+		translation *= Time.deltaTime;
+		straffe *= Time.deltaTime;
+
+		transform.Translate(straffe, 0, translation);
+
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			Cursor.lockState = (Cursor.lockState == CursorLockMode.Locked)? CursorLockMode.None : CursorLockMode.Locked;
+			print(Cursor.lockState);
+		}
+	}
+}
